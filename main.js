d671a7a5de147e1241808bc36bcd91aff67db9e5
jufxas <kd220366@gmail.com>
2022-02-15 20:27:32 -0500

    final

--- main.js
+++ main.js
@@ -30,10 +30,9 @@ function colorValue(r,g,b) {
   return r + g*256 + b*65536
 }
 
-function equalRGB(rgb1, rgb2, differenceThreshold) {
-  // return Math.abs(colorValue(rgb1.r, rgb1.g, rgb1.b) - colorValue(rgb2.r, rgb2.g, rgb2.b)) <= differenceThreshold
+function equalRGB(rgb1, rgb2) {
   return colorDifferenceScore(rgb1, rgb2) <= 0.5
-  // 0.5
+  // ? EXPERIMENTAL value 0.5; Changes in these values result in noticeable changes in song output 
 } 
 
 
@@ -72,13 +71,13 @@ function comparePalettes(paletteList1, paletteList2, maxDifference = 0.3) {
 function closestPalette(palette) {
   let paletteScores = []
   for (let i = 0; i < PlaylistsToPalette.length; i++) {
-    paletteScores.push({score: comparePalettes(palette, PlaylistsToPalette[i].palette, 0.2), id: PlaylistsToPalette[i].id})
+    paletteScores.push({score: comparePalettes(palette, PlaylistsToPalette[i].palette, 0.2), id: PlaylistsToPalette[i].id})  // ? EXPERIMENTAL value -> the comparePalettes 3rd parameter  
   }
-  // 0.4
   paletteScores.sort((a, b) => b.score - a.score)
   console.log(paletteScores)
   return paletteScores[0].id
 }
+
 
 (function initializePlaylistsToPalette() {
   for (let i = 0; i < raw.length; i++) {
@@ -89,10 +88,10 @@ function closestPalette(palette) {
   }
 })()
 
-GenerateSong("./images/omo.png", 1000)
+GenerateSong("./images/omo.png")
 
 
-async function GenerateSong(file, differenceThreshold) {
+async function GenerateSong(file) {
     await jimp.read(file, function (err, image) {
         if (err) throw err
         let avgRed = 0 
@@ -100,7 +99,6 @@ async function GenerateSong(file, differenceThreshold) {
         let avgBlue = 0 
         let colorPalette = []
         let colorAndWeight = {}
-        let sum = 0 
 
         const pixels = image.bitmap.width * image.bitmap.height
         image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
@@ -118,7 +116,7 @@ async function GenerateSong(file, differenceThreshold) {
             } else  {
               let pass = true
               for (let i = 0; i < colorPalette.length; i++) {
-                 if (equalRGB(colorPalette[i], currentPixelRGB, differenceThreshold)) {
+                 if (equalRGB(colorPalette[i], currentPixelRGB)) {
                     colorAndWeight[colorPalette[i].format()] += 1 
                     pass = false 
                     break
@@ -131,85 +129,71 @@ async function GenerateSong(file, differenceThreshold) {
             }
         })
 
-        
-        console.log(`Colors: ${Object.keys(colorAndWeight).length}`)
-
-        if (Object.keys(colorAndWeight).length >= 1000) {
-          console.log("********TOO LONG, REDO*******")
-          GenerateSong(file, differenceThreshold*10)
-          return; 
-        }
-
-          // CHANGE PALETTE SUM 
         let iterator = 0 
         let organizedPalette = []
 
         for (const color in colorAndWeight) {
-          let colorWeight = parseFloat(((colorAndWeight[color] / pixels) * 100).toFixed(2))
-          if (colorWeight >= 1) {
-            sum += colorWeight
+          let colorWeight = parseFloat(((colorAndWeight[color] / pixels)).toFixed(2))
+          if (colorWeight >= 0.01) {    // ? EXPERIMENTAL value 0.01; means that some color is 1% or more of an image 
             organizedPalette.push(new RGB( colorPalette[iterator].r, colorPalette[iterator].g, colorPalette[iterator].b ))
           }
           iterator++
         }
+
+        console.log(`Considered Colors: ${organizedPalette.length}`)
         
         avgRed /= pixels; avgBlue /= pixels; avgGreen /= pixels
-        console.log(sum)
         console.log({r: Math.round(avgRed), g: Math.round(avgGreen), b: Math.round(avgBlue)})
         console.log(organizedPalette)
+     
 
-        if (sum < 0) {  // 95 to 0 
-          console.log("********REDO*******")
-          GenerateSong(file, differenceThreshold*10)
-        } else {
+        let id = closestPalette(organizedPalette)
 
-          let id = closestPalette(organizedPalette)
-
-          const options = {
-            hostname: 'api.jamendo.com',
-            port: 443,
-            path: `/v3.0/playlists/tracks?client_id=${client_id}&id=${id}&limit=200`,
-            method: 'GET',
-          }
-          let str = ""
-          const req = https.request(options, res => {
-            console.log(`statusCode: ${res.statusCode}`)
-        
-            res.on('data', data => {
-              str += data
-            })
-            res.on('end', () => {
-              let tracks = JSON.parse(str).results[0].tracks
-              console.log(`ID: ${id}\nPLAYLIST: ${JSON.parse(str).results[0].name}`)
-              console.log(`LENGTH: ${tracks.length}`)
-
-              let avgRGBToInt = colorValue(avgRed, avgBlue, avgGreen)
-              let selectedIndex = Math.round((avgRGBToInt*(tracks.length - 1))/COLOR_VALUE_MAX)
-              let song = tracks[selectedIndex]
-
-              console.log(`SONG: ${song.name}`)
-
-              https.get(song.audiodownload, function(response) {
-                let failed = true 
-                for (let i = 0; i < response.rawHeaders.length; i++) {
-                  if (response.rawHeaders[i].includes("attachment;")) failed = false 
-                }
-                if (failed) console.log(`Request for ${song.name} failed.`)
-                else {
-                  console.log(`Request for ${song.name} succeeded.`)
-                  response.pipe(songFile);
-                }
-              });
-
-            })
-          })
-          req.on('error', error => {
-            console.error(error)
-          })
-          req.end()
-
-
+        const options = {
+          hostname: 'api.jamendo.com',
+          port: 443,
+          path: `/v3.0/playlists/tracks?client_id=${client_id}&id=${id}&limit=200`,
+          method: 'GET',
         }
+        let str = ""
+        const req = https.request(options, res => {
+          console.log(`statusCode: ${res.statusCode}`)
+      
+          res.on('data', data => {
+            str += data
+          })
+          res.on('end', () => {
+            console.log(`ID: ${id}\nPLAYLIST: ${JSON.parse(str).results[0].name}`)
+            console.log(`LENGTH: ${tracks.length}`)
+
+            let tracks = JSON.parse(str).results[0].tracks
+            let avgRGBToInt = colorValue(avgRed, avgBlue, avgGreen)
+            let selectedIndex = Math.round((avgRGBToInt*(tracks.length - 1))/COLOR_VALUE_MAX)
+            let song = tracks[selectedIndex]
+
+            console.log(`SONG: ${song.name}`)
+
+            https.get(song.audiodownload, function(response) {
+              let failed = true 
+              for (let i = 0; i < response.rawHeaders.length; i++) {
+                if (response.rawHeaders[i].includes("attachment;")) failed = false 
+              }
+              if (failed) console.log(`Request for ${song.name} failed.`)
+              else {
+                console.log(`Request for ${song.name} succeeded.`)
+                response.pipe(songFile);
+              }
+            });
+
+          })
+        })
+        req.on('error', error => {
+          console.error(error)
+        })
+        req.end()
+
+
+        
     });
 }
 
